acc4af7ebe71270276f64b5cbece9cf96ebeec2c
Flawbert Costa <108291636+flawbert@users.noreply.github.com>
2023-06-19 11:36:54 -0300

    Update propriedades_dos_numeros.py

--- AtividadesPC/Repeticao_e_formas_de_representacao/propriedades_dos_numeros.py
+++ AtividadesPC/Repeticao_e_formas_de_representacao/propriedades_dos_numeros.py
@@ -1,5 +1,6 @@
 #Múltiplos de 6
 numeros = []
+numero = []
 n = int(input("Insira um número: "))
 print("Múltiplos de 6 menores que o número escolhido: ")
 for i in range(1, n+1):
@@ -7,4 +8,7 @@ for i in range(1, n+1):
         numeros.append(i)
         print(numeros)
 print("O múltiplo de 6 após o número escolhido: ")
-
+for i in range(n+1, n+7):
+    if i%2 == 0 and i%3 ==0:
+        numero.append(i)
+        print(numero)
