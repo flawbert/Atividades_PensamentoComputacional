729b1b314ddf51f9e98d419c716c2b1d5b00aa07
Flawbert Costa <108291636+flawbert@users.noreply.github.com>
2023-06-19 12:02:07 -0300

    Update propriedades_dos_numeros.py

--- AtividadesPC/Repeticao_e_formas_de_representacao/propriedades_dos_numeros.py
+++ AtividadesPC/Repeticao_e_formas_de_representacao/propriedades_dos_numeros.py
@@ -12,3 +12,21 @@ for i in range(n+1, n+7):
     if i%2 == 0 and i%3 ==0:
         numero.append(i)
         print(numero)
+#------------------------------------------------------------
+
+#Primo ou Composto
+def verificar_primalidade(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True 
+n = int(input("Insira um número: "))
+if verificar_primalidade(n):
+    print(n, "é um número primo.")
+elif n < 2:
+  print("Números menores do que 2 não são primos! ")
+else:
+    print(n, "é um número composto.")
+         
